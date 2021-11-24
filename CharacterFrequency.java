cc52f7f2cc23bff700fae1b5a92e7162c0e7f648
Carlos <ineedzpancakes@gmail.com>
2021-11-24 12:33:57 -0600

    updated methods

--- CharacterFrequency.java
+++ CharacterFrequency.java
@@ -3,9 +3,10 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Scanner;
 
-class CharacterFrequency {
-    static String fileName;
-    public static char[][] ch2D = {
+public class CharacterFrequency {
+    public String fileName;
+    public HashMap<Character, Integer> charFreq;
+    public static final char[][] ch2D = {
             { '`', '~', '1', '!', '2', '@', '3', '#', '4', '$', '5', '%', '6', '^', '7', '&', '8', '*', '9', '(', '0',
                     ')', '-', '_', '=', '+' },
             { 'q', 'Q', 'w', 'W', 'e', 'E', 'r', 'R', 't', 'T', 'y', 'Y', 'u', 'U', 'i', 'I', 'o', 'O', 'p', 'P', '[',
@@ -15,94 +16,84 @@ class CharacterFrequency {
             { 'z', 'Z', 'x', 'X', 'c', 'C', 'v', 'V', 'b', 'B', 'n', 'N', 'm', 'M', ',', '<', '.', '>', '/', '?' } };
 
     public CharacterFrequency(String fileName) {
-        CharacterFrequency.fileName = fileName;
+        this.fileName = fileName;
+        try {
+            charFreq = getCharacterFrequency();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    // Method to read the file and store the characters in a HashMap
     public HashMap<Character, Integer> getCharacterFrequency() throws IOException {
-        HashMap<Character, Integer> characterFrequency = new HashMap<>();
-        File file = new File(fileName);
-        Scanner sc = new Scanner(file);
-        while (sc.hasNextLine()) {
-            String str = sc.nextLine();
+        HashMap<Character, Integer> charFreq = new HashMap<Character, Integer>();
+        Scanner scanner = new Scanner(new File(fileName));
+        while (scanner.hasNextLine()) {
+            String str = scanner.nextLine();
             for (int i = 0; i < str.length(); i++) {
                 char c = str.charAt(i);
-                if (characterFrequency.containsKey(c)) {
+                if (charFreq.containsKey(c)) {
                     // Increment frequency of character count
-                    characterFrequency.put(c, characterFrequency.get(c) + 1);
+                    charFreq.put(c, charFreq.get(c) + 1);
                 } else {
                     // If no character count found, add character count
-                    characterFrequency.put(c, 1);
+                    charFreq.put(c, 1);
                 }
             }
         }
-        sc.close();
-        return characterFrequency;
+        scanner.close();
+        return charFreq;
     }
 
-    // Returns the total amount of keys pressed
-    public int totalKeysPressed() throws IOException {
-        int sum = 0;
-        for (int i : getCharacterFrequency().values()) {
-            sum += i;
-        }
-        return sum;
-    }
-
-    // Returns the highest frequency
-    public int getMaxUniqueFrequency() throws IOException {
-        int maxFrequency = 0;
-        for (int i : getCharacterFrequency().values()) {
-            if (i > maxFrequency) {
-                maxFrequency = i;
-            }
-        }
-        return maxFrequency;
-    }
-
-    // Returns the lowest frequency
-    public int getMinUniqueFrequency() throws IOException {
-        int minFrequency = Integer.MAX_VALUE;
-        for (int i : getCharacterFrequency().values()) {
-            if (i < minFrequency) {
-                minFrequency = i;
-            }
-        }
-        return minFrequency;
-    }
-
-    // Returns the highest frequency of a key
-    public int getMaxKeyFrequency() throws IOException {
-        int maxFrequency = 0;
+    public int getMaxFrequency() {
+        int freq;
+        int max = 0;
         for (int i = 0; i < ch2D.length; i++) {
             for (int j = 0; j < ch2D[i].length / 2; j++) {
-                if (getSingleFrequency(ch2D[i][2 * j]) + getSingleFrequency(ch2D[i][2 * j + 1]) > maxFrequency) {
-                    maxFrequency = getSingleFrequency(ch2D[i][2 * j]) + getSingleFrequency(ch2D[i][2 * j + 1]);
+                freq = 0;
+                if (charFreq.containsKey(ch2D[i][j * 2])) {
+                    freq += charFreq.get(ch2D[i][j * 2]);
+                }
+                if (charFreq.containsKey(ch2D[i][j * 2 + 1])) {
+                    freq += charFreq.get(ch2D[i][j * 2 + 1]);
+                }
+                if (freq > max) {
+                    max = freq;
                 }
             }
         }
-        return maxFrequency;
+        return max;
     }
 
-    // Returns the lowest frequency of a key
-    public int getMinKeyFrequency() throws IOException {
-        int minFrequency = Integer.MAX_VALUE;
+    public int getMinFrequency() {
+        int freq;
+        int min = Integer.MAX_VALUE;
         for (int i = 0; i < ch2D.length; i++) {
             for (int j = 0; j < ch2D[i].length / 2; j++) {
-                if (getSingleFrequency(ch2D[i][2 * j]) + getSingleFrequency(ch2D[i][2 * j + 1]) < minFrequency) {
-                    minFrequency = getSingleFrequency(ch2D[i][2 * j]) + getSingleFrequency(ch2D[i][2 * j + 1]);
+                freq = 0;
+                if (charFreq.containsKey(ch2D[i][j * 2])) {
+                    freq += charFreq.get(ch2D[i][j * 2]);
+                }
+                if (charFreq.containsKey(ch2D[i][j * 2 + 1])) {
+                    freq += charFreq.get(ch2D[i][j * 2 + 1]);
+                }
+                if (freq < min) {
+                    min = freq;
                 }
             }
         }
-        return minFrequency;
+        return min;
     }
 
-    // Returns the frequency of a given chacter
-    public int getSingleFrequency(char c) throws IOException {
-        if (getCharacterFrequency().containsKey(c)) {
-            return getCharacterFrequency().get(c);
-        } else {
-            return 0;
+    public int getKeyFrequency(char index1, char index2) {
+        int freq = 0;
+        if (charFreq.containsKey(index1)) {
+            freq += charFreq.get(index1);
         }
+        if (charFreq.containsKey(index2)) {
+            freq += charFreq.get(index2);
+        }
+        return freq;
     }
+
 }
--- KeyboardHeatmap.java
+++ KeyboardHeatmap.java
@@ -6,29 +6,27 @@ import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.AlphaComposite;
 import java.io.File;
-import java.io.IOException;
 import java.util.Scanner;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 class Surface extends JPanel {
-
     private Image keyboard;
-    static CharacterFrequency characterFrequency;
-    char[][] ch2D = CharacterFrequency.ch2D;
+    final char[][] ch2D = CharacterFrequency.ch2D;
+    CharacterFrequency charFreq;
 
     public Surface() {
         // Constantly checks for a valid file until one is found or user quits
         while (true) {
             System.out.print("Please enter the file you would like to scan: ");
-            Scanner sc = new Scanner(System.in);
-            String fileName = sc.nextLine();
+            Scanner scanner = new Scanner(System.in);
+            String fileName = scanner.nextLine();
             if (new File(fileName).exists()) {
                 // make new CharacterFrequency object
-                characterFrequency = new CharacterFrequency(fileName);
+                charFreq = new CharacterFrequency(fileName);
                 System.out.println("File Found. Click the Java icon on your taskbar to see the heatmap.");
-                sc.close();
+                scanner.close();
                 break;
             } else {
                 System.out.println("File not found. Try again or CTRL + C to exit.");
@@ -51,7 +49,7 @@ class Surface extends JPanel {
         setPreferredSize(d);
     }
 
-    private void colorHeatmap(Graphics g) throws IOException {
+    private void colorHeatmap(Graphics g) {
 
         Graphics2D g2d = (Graphics2D) g;
         g2d.drawImage(keyboard, 0, 0, null);
@@ -60,15 +58,15 @@ class Surface extends JPanel {
         int yPosition = 20; // y position of the first key
         int width = 72; // width of each key
         int height = 68; // height of each key
-        float alpha = 5 * 0.1f; // transparency of the key
+        float alpha = 6 * 0.1f; // transparency of the key
 
+        int min = charFreq.getMinFrequency();
+        int max = charFreq.getMaxFrequency();
         for (int i = 0; i < ch2D.length; i++) {
             for (int j = 0; j < ch2D[i].length / 2; j++) {
                 // ch2D[2 * j] and ch2D[2 * j + 1] are the two characters in a single key
-                int val = characterFrequency.getSingleFrequency(ch2D[i][2 * j])
-                        + characterFrequency.getSingleFrequency(ch2D[i][2 * j + 1]);
-                g2d.setColor(calculateColor(val, characterFrequency.getMinKeyFrequency(),
-                        characterFrequency.getMaxKeyFrequency()));
+                int val = charFreq.getKeyFrequency(ch2D[i][j * 2], ch2D[i][j * 2 + 1]);
+                g2d.setColor(calculateColor(val, min, max));
                 g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, alpha));
                 if (i == 0) { // first row (top row)
                     g2d.fillRect(xPosition, yPosition, width, height);
@@ -103,65 +101,23 @@ class Surface extends JPanel {
         }
     }
 
-    // Retuns a color based on the value of the key
-    // I couldn't figure out a better way to do this
     public Color calculateColor(int value, int minimum, int maximum) {
         double percent = ((value - minimum) * 100) / (maximum - minimum);
-        if (percent >= 94.44444444444444) {
-            return new Color(124, 10, 1);
-        } else if (94.44444444444444 > percent && percent >= 88.88888888888889) {
-            return new Color(208, 26, 15);
-        } else if (88.88888888888889 > percent && percent >= 83.33333333333333) {
-            return new Color(253, 57, 0);
-        } else if (83.33333333333333 > percent && percent >= 77.77777777777777) {
-            return new Color(252, 185, 19);
-        } else if (77.77777777777777 > percent && percent >= 72.22222222222221) {
-            return new Color(254, 251, 1);
-        } else if (72.22222222222221 > percent && percent >= 66.66666666666666) {
-            return new Color(255, 251, 94);
-        } else if (66.66666666666666 > percent && percent >= 61.1111111111111) {
-            return new Color(206, 251, 2);
-        } else if (61.1111111111111 > percent && percent >= 55.55555555555554) {
-            return new Color(135, 250, 0);
-        } else if (55.55555555555554 > percent && percent >= 49.999999999999986) {
-            return new Color(58, 249, 1);
-        } else if (49.999999999999986 > percent && percent >= 44.44444444444443) {
-            return new Color(0, 255, 0);
-        } else if (44.44444444444443 > percent && percent >= 38.88888888888887) {
-            return new Color(0, 229, 75);
-        } else if (38.88888888888887 > percent && percent >= 33.333333333333314) {
-            return new Color(0, 212, 176);
-        } else if (33.333333333333314 > percent && percent >= 27.777777777777757) {
-            return new Color(0, 183, 216);
-        } else if (27.777777777777757 > percent && percent >= 22.2222222222222) {
-            return new Color(24, 138, 240);
-        } else if (22.2222222222222 > percent && percent >= 16.666666666666643) {
-            return new Color(20, 108, 246);
-        } else if (16.666666666666643 > percent && percent >= 11.111111111111088) {
-            return new Color(0, 82, 255);
-        } else if (11.111111111111088 > percent && percent >= 5.555555555555532) {
-            return new Color(0, 0, 255);
-        } else if (5.555555555555532 > percent && percent > 0.0) {
-            return new Color(7, 0, 196);
-        } else {
-            return new Color(0, 0, 0);
-        }
+        Color[] colors = { new Color(255, 255, 217), new Color(237, 248, 177), new Color(199, 233, 180),
+                new Color(127, 205, 187), new Color(66, 182, 196), new Color(29, 145, 192), new Color(33, 95, 168),
+                new Color(37, 52, 148), new Color(7, 30, 88) };
+        int index = (int) Math.floor(percent / 100 * (colors.length - 1));
+        return colors[index];
     }
 
     @Override
     public void paintComponent(Graphics g) {
-
         super.paintComponent(g);
-        try {
-            colorHeatmap(g);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        colorHeatmap(g);
     }
 }
 
 public class KeyboardHeatmap extends JFrame {
-
     public KeyboardHeatmap() {
 
         initUI();
